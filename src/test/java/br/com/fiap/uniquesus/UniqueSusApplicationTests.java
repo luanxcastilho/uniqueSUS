537d77f191a44062fa409f09e15e7c8e12d8f8cb
Luan Monte Ferreri Castilho <luan.castilho@outlook.com.br>
2025-08-28 22:29:50 -0300

    Primeiro commit no repo novo

---
+++ src/test/java/br/com/fiap/uniquesus/UniqueSusApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.fiap.uniquesus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueSusApplicationTests
+{
+    
+    @Test
+    void contextLoads ()
+    {
+    }
+    
+}
